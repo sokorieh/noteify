0be8bec00dab0777511c6bb04fb0e7bc80bedfac
heirokos <ericericsouza02@gmail.com>
2023-12-31 19:21:17 -0500

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      perspective: {
+        '2000': '2000px',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
